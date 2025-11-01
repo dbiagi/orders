444aa9bacacd0da6dfb58d4b969c88eb1e3d0213
Diego de Biagi <diegobiagi@protonmail.com>
2025-11-01 10:15:28 -0300

    fix: update controller

--- src/main/java/com/dbiagi/demo/controller/OrderController.java
+++ src/main/java/com/dbiagi/demo/controller/OrderController.java
@@ -4,21 +4,28 @@ import com.dbiagi.demo.domain.OrderRequest;
 import com.dbiagi.demo.domain.OrderResponse;
 import com.dbiagi.demo.domain.OrderUpdateRequest;
 import com.dbiagi.demo.service.OrderCreateService;
+import com.dbiagi.demo.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
     private final OrderCreateService orderCreateService;
+    private final OrderService orderService;
 
-    OrderController(OrderCreateService orderCreateService) {
+    OrderController(OrderCreateService orderCreateService, OrderService orderService) {
         this.orderCreateService = orderCreateService;
+        this.orderService = orderService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public OrderResponse createOrder(@RequestBody OrderRequest request) {
         try {
             return orderCreateService.create(request);
@@ -27,7 +34,9 @@ public class OrderController {
         }
     }
 
+    @PatchMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateOrder(@RequestBody OrderUpdateRequest request) {
-
+        orderService.update(request);
     }
 }
